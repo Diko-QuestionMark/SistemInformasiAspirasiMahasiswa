9999299d4d5ee4495556c92e93f326dcdcdaeb96
Feed <fedrikosetiawanlp@gmail.com>
2026-06-30 08:24:00 +0700

    yud pull kan

---
+++ test-db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Connection error', err.stack);
+  } else {
+    console.log('Connected! Time:', res.rows[0].now);
+  }
+  pool.end();
+});
